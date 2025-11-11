f722a1886b26c9179d508523c91493d0a6375728
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-11-11 06:31:15 -0300

    Inicializando com o Spring Initializr

---
+++ threadsvirtuais/src/main/java/com/cin/threadsvirtuais/ThreadsvirtuaisApplication.java
@@ -0,0 +1,13 @@
+package com.cin.threadsvirtuais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsvirtuaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsvirtuaisApplication.class, args);
+	}
+
+}
---
+++ threadsvirtuais/src/test/java/com/cin/threadsvirtuais/ThreadsvirtuaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cin.threadsvirtuais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsvirtuaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
